4bcb4326b4821d1ae693693c85d0d685638f66b9
BStrang213 <144375601+BStrang213@users.noreply.github.com>
2026-08-19 21:30:40 -0230

    No real updates but the app seems to be running now.

--- app/src/main/java/com/example/cp_4282_project/MainActivity.kt
+++ app/src/main/java/com/example/cp_4282_project/MainActivity.kt
@@ -11,11 +11,11 @@ import android.widget.EditText
 class MainActivity : ComponentActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.team_roster)
+//        setContentView(R.layout.team_roster)
 //        setContentView(R.layout.player_card)
-//        setContentView(R.layout.user_guide)
-        val button1: Button = findViewById(R.id.teamButton)
-        button1.setOnClickListener(this)
+        setContentView(R.layout.user_guide)
+//        val button1: Button = findViewById(R.id.teamButton)
+//        button1.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
