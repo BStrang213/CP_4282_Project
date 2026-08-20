4a2c65b406aeeebfec859ebdb79add763f9fb0c1
bradley.strang <Bradley.strang07@ed.cna.nl.ca>
2026-08-20 16:43:38 -0230

    The program now swaps between screens when a button is pressed, however, Unable to get SQLite to work. Unable to get the data to update in the program.

--- app/src/main/java/com/example/cp_4282_project/MainActivity.kt
+++ app/src/main/java/com/example/cp_4282_project/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.cp_4282_project
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import android.widget.TextView
@@ -12,10 +13,18 @@ class MainActivity : ComponentActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.team_roster)
-//        setContentView(R.layout.player_card)
-//        setContentView(R.layout.user_guide)
         val button1: Button = findViewById(R.id.teamButton)
-        button1.setOnClickListener(this)
+        val button2: Button = findViewById(R.id.InfoPage)
+        button1.setOnClickListener {
+            val intent = Intent()
+            intent.setClass(this, PlayerCard::class.java)
+            startActivity(intent)
+        }
+        button2.setOnClickListener {
+            val intent = Intent()
+            intent.setClass(this, UserGuide::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onClick(v: View?) {
